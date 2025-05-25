85f6d1d8231d13ae07ce972f6a9145effc54a4d9
Ben Rowe <ben.rowe.83@gmail.com>
2025-05-26 07:27:10 +1000

    Add additional endpoints and logging

--- main.go
+++ main.go
@@ -3,15 +3,39 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
+
+func logRequest(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Log the request details
+		fmt.Printf("[%s] %s %s\n", time.Now().Format(time.RFC3339), r.Method, r.URL.Path)
+
+		// Call the next handler
+		next.ServeHTTP(w, r)
+	})
+}
 
 func main() {
 	port := 1234
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello, World!"))
+	mux := http.NewServeMux()
+
+	static := http.FileServer(http.Dir("./web/src/static-html"))
+	mux.Handle("/", static)
+
+	assetsDir := http.Dir("./web/dist/")
+	assetsHandler := http.StripPrefix("/assets/", http.FileServer(assetsDir))
+	mux.Handle("/assets/", assetsHandler)
+
+	mux.HandleFunc("/time", func(w http.ResponseWriter, r *http.Request) {
+		currentTime := time.Now().Format(time.RFC3339Nano)
+		fmt.Fprintf(w, "Current time: %s", currentTime)
 	})
-	fmt.Println(fmt.Sprintf("Server started on http://localhost:%d", port))
-	if err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil); err != nil {
+	loggedMux := logRequest(mux)
+
+	fmt.Printf("Server started on http://localhost:%d\n", port)
+	if err := http.ListenAndServe(fmt.Sprintf(":%d", port), loggedMux); err != nil {
 		panic(err)
 	}
 }
+
